b751132cf697c14cc00f5cba16fc43066d6a5017
Kelvin Muyambo <kelvinm@afrosoft.co.zw>
2018-10-31 16:56:21 +0200

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicesComponent } from './components/services/services.component';
+import { IndustriesComponent } from './components/industries/industries.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CommunityComponent } from './components/community/community.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    FooterComponent,
+    ServicesComponent,
+    IndustriesComponent,
+    ProductsComponent,
+    CommunityComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'industries', component: IndustriesComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'community', component: CommunityComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
